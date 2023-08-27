30a24d7e2bdbb17422b34cf997693762786c1f16
anhinga <anhinga@users.noreply.github.com>
2023-08-27 16:52:44 -0400

    Update immutable_machine.py

--- August-2023/immutable_machine.py
+++ August-2023/immutable_machine.py
@@ -11,3 +11,31 @@ initial_output = {'self': add_v_values(init_matrix, {':function': {'accum_add_ar
                   'update-1': add_v_values(update_1({}), {':function': {'update_1': 1.0}}),
                   'update-2': add_v_values(update_2({}), {':function': {'update_2': 1.0}}),
                   'update-3': add_v_values(update_3({}), {':function': {'update_3': 1.0}})}
+
+from pprint import pprint
+
+pprint(init_matrix)
+# {'result': {'self': {':function': {'self': {':function': 1.0}},
+#                      'accum': {'self': {'result': 1.0}},
+#                      'delta': {'update-1': {'result': 1.0}}},
+#             'update-1': {':function': {'update-1': {':function': 1.0}}},
+#             'update-2': {':function': {'update-2': {':function': 1.0}}},
+#             'update-3': {':function': {'update-3': {':function': 1.0}}}}}
+
+pprint(initial_output)
+# {'self': {':function': {'accum_add_args': 1.0},
+#           'result': {'self': {':function': {'self': {':function': 1.0}},
+#                               'accum': {'self': {'result': 1.0}},
+#                               'delta': {'update-1': {'result': 1.0}}},
+#                      'update-1': {':function': {'update-1': {':function': 1.0}}},
+#                      'update-2': {':function': {'update-2': {':function': 1.0}}},
+#                      'update-3': {':function': {'update-3': {':function': 1.0}}}}},
+#  'update-1': {':function': {'update_1': 1.0},
+#               'result': {'self': {'delta': {'update-1': {'result': -1.0},
+#                                             'update-2': {'result': 1.0}}}}},
+#  'update-2': {':function': {'update_2': 1.0},
+#               'result': {'self': {'delta': {'update-2': {'result': -1.0},
+#                                             'update-3': {'result': 1.0}}}}},
+#  'update-3': {':function': {'update_3': 1.0},
+#               'result': {'self': {'delta': {'update-1': {'result': 1.0},
+#                                             'update-3': {'result': -1.0}}}}}}
