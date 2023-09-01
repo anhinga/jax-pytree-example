9e73cacbe8949d1e3ea3aceb590fcc4f9ae3a292
anhinga <michael.bukatin@gmail.com>
2023-09-01 09:46:37 -0400

    bug fixes; computes some num_grads now

--- September-2023/immutable_num_grad.py
+++ September-2023/immutable_num_grad.py
@@ -6,20 +6,20 @@
 from jax.tree_util import tree_map_with_path
 
 def num_grad(loss, tree):
-    # for JAX paths which are DictKey(key='string') and for our "tree"
-    def update_tree_at_path(path, func_leaf):
+    # for JAX paths which are DictKey(key='string')
+    def update_tree_at_path(tree, path, func_leaf):
         if len(path) == 1:
             return {k: func_leaf(v) if k == path[0].key else v for k, v in tree.items()}
         else:
             key, *rest_path = path
             return {k: update_tree_at_path(v, rest_path, func_leaf) if k == key.key else v for k, v in tree.items()}
-    # compute one partial derivative for our "loss" (should we parametrize this to compute one-sided ones also?)
+    # compute one partial derivative for our "loss" and "tree' (should we parametrize this to compute one-sided ones also?)
     def compute_partial_derivative(path, leaf):
         delta = 0.01
-        tree1 = update_tree_at_path(path, lambda v: v+delta)
-        tree2 = update_tree_at_path(path, lambda v: v-delta)
+        tree1 = update_tree_at_path(tree, path, lambda v: v+delta)
+        tree2 = update_tree_at_path(tree, path, lambda v: v-delta)
         return (loss(tree1) - loss(tree2))/(2*delta)
-    return tree_map_with_path(loss, tree)
+    return tree_map_with_path(compute_partial_derivative, tree)
 
 
         
